8f117026d5b05e7f10f5c843e7a2860abcbcdc00
Nick Hester <nick_hester@hotmail.com>
2016-11-09 12:20:06 -0600

    added player and inventory

---
+++ TileMiner/Assets/Scripts/Inventory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory
+{
+	public int numDirt = 0;
+	public int numStone = 0;
+	public int numDiamonds = 0;
+}
---
+++ TileMiner/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+	private Inventory myInventory;
+
+	void Start ()
+	{
+		myInventory = new Inventory();
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
